193362373bba0e114e8e223f3ca63371389d00b4
ak5k <42914711+ak5k@users.noreply.github.com>
2024-06-11 14:18:32 +0300

    chore: Remove unused PluginEditor, LevelMeter, SoftClipper classes, and unnecessary return statements

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,188 +1,388 @@
 #include "PluginProcessor.h"
+
 #include "PluginEditor.h"
+#include "SoftClipper.h"
 
 //==============================================================================
-AudioPluginAudioProcessor::AudioPluginAudioProcessor()
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+RipuLimiterAudioProcessor::RipuLimiterAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+    : AudioProcessor(BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+                         .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+                         .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+      )
+#endif
+    , apvts(*this, nullptr, "PARAMETERS", createParameterLayout())
 {
 }
 
-AudioPluginAudioProcessor::~AudioPluginAudioProcessor()
+RipuLimiterAudioProcessor::~RipuLimiterAudioProcessor() = default;
+
+juce::AudioProcessorValueTreeState::ParameterLayout RipuLimiterAudioProcessor::createParameterLayout()
 {
+    std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
+
+    {
+        auto attributes = AudioParameterFloatAttributes().withLabel(" dB");
+        params.push_back(std::make_unique<AudioParameterFloat>(
+            "thresh", "Threshold", juce::NormalisableRange<float>(-24.0f, 0.0f, 0.1f), -0.1f, attributes
+        ));
+    }
+
+    {
+        auto attributes = AudioParameterFloatAttributes().withLabel(" dB");
+        params.push_back(std::make_unique<AudioParameterFloat>(
+            "gain", "Gain", juce::NormalisableRange<float>(-24.0f, 0.0f, 0.1f), -1.0f, attributes
+        ));
+    }
+
+    {
+        auto attributes = AudioParameterFloatAttributes().withLabel(" dB");
+        params.push_back(std::make_unique<AudioParameterFloat>(
+            "drive", "Drive", juce::NormalisableRange<float>(0.0f, 24.0f, 0.1f), 0.0f, attributes
+        ));
+    }
+
+    {
+        auto attributes = AudioParameterFloatAttributes().withLabel(" dB");
+        params.push_back(std::make_unique<AudioParameterFloat>(
+            "knee", "Knee", juce::NormalisableRange<float>(0.0f, 6.0f, 0.1f), 0.0f, attributes
+        ));
+    }
+
+    {
+        auto attributes = AudioParameterFloatAttributes().withLabel(" ms");
+        params.push_back(std::make_unique<AudioParameterFloat>(
+            "hold", "Hold", juce::NormalisableRange<float>(0.0f, 20.0f, 0.1f), 1.0f, attributes
+        ));
+    }
+
+    {
+        auto attributes = AudioParameterFloatAttributes().withLabel(" ms");
+        params.push_back(std::make_unique<AudioParameterFloat>(
+            "release", "Release", juce::NormalisableRange<float>(5.0f, 300.0f, 0.1f, 0.38f), 5.0f, attributes
+        ));
+    }
+
+    params.push_back(std::make_unique<juce::AudioParameterBool>("link", "Link", false));
+    params.push_back(std::make_unique<juce::AudioParameterBool>("oversample", "Oversample", false));
+    params.push_back(std::make_unique<juce::AudioParameterBool>("cascade", "Cascade", true));
+    params.push_back(std::make_unique<juce::AudioParameterBool>("deEsser", "De-Esser", false));
+
+    return {params.begin(), params.end()};
 }
 
 //==============================================================================
-const juce::String AudioPluginAudioProcessor::getName() const
+const juce::String RipuLimiterAudioProcessor::getName() const
 {
     return JucePlugin_Name;
 }
 
-bool AudioPluginAudioProcessor::acceptsMidi() const
+bool RipuLimiterAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
-bool AudioPluginAudioProcessor::producesMidi() const
+bool RipuLimiterAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
-bool AudioPluginAudioProcessor::isMidiEffect() const
+bool RipuLimiterAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
-double AudioPluginAudioProcessor::getTailLengthSeconds() const
+double RipuLimiterAudioProcessor::getTailLengthSeconds() const
 {
     return 0.0;
 }
 
-int AudioPluginAudioProcessor::getNumPrograms()
+int RipuLimiterAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1; // NB: some hosts don't cope very well if you tell them there are 0 programs,
+    // so this should be at least 1, even if you're not really implementing programs.
 }
 
-int AudioPluginAudioProcessor::getCurrentProgram()
+int RipuLimiterAudioProcessor::getCurrentProgram()
 {
     return 0;
 }
 
-void AudioPluginAudioProcessor::setCurrentProgram (int index)
+void RipuLimiterAudioProcessor::setCurrentProgram(int index)
 {
-    juce::ignoreUnused (index);
 }
 
-const juce::String AudioPluginAudioProcessor::getProgramName (int index)
+const juce::String RipuLimiterAudioProcessor::getProgramName(int index)
 {
-    juce::ignoreUnused (index);
     return {};
 }
 
-void AudioPluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void RipuLimiterAudioProcessor::changeProgramName(int index, const juce::String& newName)
 {
-    juce::ignoreUnused (index, newName);
 }
 
 //==============================================================================
-void AudioPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void RipuLimiterAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-    juce::ignoreUnused (sampleRate, samplesPerBlock);
+    auto numChannels = getTotalNumInputChannels();
+    if (numChannels == 0)
+        return;
+
+    limiters.resize(numChannels);
+    for (auto& limiter : limiters)
+        limiter.configure(sampleRate);
+
+    thresholdSmoothed.resize(numChannels);
+    gainSmoothed.resize(numChannels);
+    driveSmoothed.resize(numChannels);
+    kneeSmoothed.resize(numChannels);
+    holdSmoothed.resize(numChannels);
+    releaseSmoothed.resize(numChannels);
+
+    for (auto& i : thresholdSmoothed)
+        i.reset(sampleRate, 0.1);
+    for (auto& i : gainSmoothed)
+        i.reset(sampleRate, 0.1);
+    for (auto& i : driveSmoothed)
+        i.reset(sampleRate, 0.1);
+    for (auto& i : kneeSmoothed)
+        i.reset(sampleRate, 0.1);
+    for (auto& i : holdSmoothed)
+        i.reset(sampleRate, 0.1);
+    for (auto& i : releaseSmoothed)
+        i.reset(sampleRate, 0.1);
+
+    oversampling = std::make_unique<juce::dsp::Oversampling<double>>(
+        numChannels, 4, juce::dsp::Oversampling<double>::filterHalfBandPolyphaseIIR, true
+    );
+    oversampling->initProcessing(samplesPerBlock);
+
+    tempBufferDouble.setSize(numChannels, samplesPerBlock);
+
+    delayLine.prepare(
+        {sampleRate,
+         (uint32_t)(limiters[0].latencySamples() + oversampling->getLatencyInSamples()),
+         (uint32_t)numChannels}
+    );
+
+    bool isOversampled = (bool)apvts.getRawParameterValue("oversample")->load();
+    if (isOversampled)
+        setLatencySamples((int)(limiters[0].latencySamples() + oversampling->getLatencyInSamples()));
+    else
+        setLatencySamples(limiters[0].latencySamples());
+
+    delayLine.setMaximumDelayInSamples(getLatencySamples());
+    delayLine.setDelay(getLatencySamples());
+    delayLine.reset();
 }
 
-void AudioPluginAudioProcessor::releaseResources()
+void RipuLimiterAudioProcessor::releaseResources()
 {
     // When playback stops, you can use this as an opportunity to free up any
     // spare memory, etc.
 }
 
-bool AudioPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+#ifndef JucePlugin_PreferredChannelConfigurations
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+bool RipuLimiterAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
+{
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
+    return true;
+#else
+#if !JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
+#endif
 
     return true;
-  #endif
+#endif
 }
 
-void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
-                                              juce::MidiBuffer& midiMessages)
-{
-    juce::ignoreUnused (midiMessages);
+#endif
 
+template <typename T>
+void RipuLimiterAudioProcessor::processBlockInternal(
+    juce::AudioBuffer<T>& bufferIn, [[maybe_unused]] juce::MidiBuffer& midiMessages
+)
+{
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+        bufferIn.clear(i, 0, bufferIn.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    auto threshold = juce::Decibels::decibelsToGain(apvts.getRawParameterValue("thresh")->load());
+    auto gain = juce::Decibels::decibelsToGain(apvts.getRawParameterValue("gain")->load());
+    auto drive = juce::Decibels::decibelsToGain(apvts.getRawParameterValue("drive")->load());
+
+    auto knee = apvts.getRawParameterValue("knee")->load();
+    auto hold = apvts.getRawParameterValue("hold")->load();
+    auto release = apvts.getRawParameterValue("release")->load();
+
+    bool isLinked = (bool)apvts.getRawParameterValue("link")->load();
+    bool isOversampled = (bool)apvts.getRawParameterValue("oversample")->load();
+    bool cascade = (bool)apvts.getRawParameterValue("cascade")->load();
+    bool deEss = (bool)apvts.getRawParameterValue("deEsser")->load();
+    for (auto& limiter : limiters)
     {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
+        limiter.setCascade(cascade);
+        limiter.setDeEsser(deEss);
+    }
+
+    for (auto& i : thresholdSmoothed)
+        i.setTargetValue(threshold);
+    for (auto& i : gainSmoothed)
+        i.setTargetValue(gain);
+    for (auto& i : driveSmoothed)
+        i.setTargetValue(drive);
+    for (auto& i : kneeSmoothed)
+        i.setTargetValue(knee);
+    for (auto& i : holdSmoothed)
+        i.setTargetValue(hold);
+    for (auto& i : releaseSmoothed)
+        i.setTargetValue(release);
+
+    juce::dsp::AudioBlock<T> block(bufferIn);
+    juce::dsp::AudioBlock<T> oversampledBlock;
+
+    bool doSaturation = !approximatelyEqual(drive, 1.0f);
+
+    bool doOverSample = isOversampled && doSaturation;
+
+    if (doOverSample)
+        oversampledBlock = oversampling->processSamplesUp(block);
+    else
+        oversampledBlock = block;
+
+    if (doSaturation)
+        for (auto channel = 0; channel < oversampledBlock.getNumChannels(); channel++)
+        {
+            auto* channelData = oversampledBlock.getChannelPointer(channel);
+            for (auto sample = 0; sample < oversampledBlock.getNumSamples(); sample++)
+            {
+                drive = driveSmoothed[channel].getNextValue();
+                knee = kneeSmoothed[channel].getNextValue();
+                channelData[sample] = SoftClipper(channelData[sample], (T)drive, (T)knee);
+            }
+        }
+
+    if (doOverSample)
+    {
+        oversampling->processSamplesDown(block);
+        setLatencySamples((int)(limiters[0].latencySamples() + oversampling->getLatencyInSamples()));
+    }
+    else
+    {
+        setLatencySamples(limiters[0].latencySamples());
+    }
+
+    if ((int)delayLine.getDelay() != getLatencySamples())
+    {
+        if (delayLine.getMaximumDelayInSamples() < getLatencySamples())
+            delayLine.setMaximumDelayInSamples(getLatencySamples());
+        delayLine.setDelay(getLatencySamples());
+        delayLine.reset();
+    }
+
+    for (int channel = 0; channel < block.getNumChannels(); ++channel)
+    {
+        auto* channelData = block.getChannelPointer(channel);
+        for (int sample = 0; sample < block.getNumSamples(); ++sample)
+        {
+            delayLine.pushSample(channel, static_cast<double>(channelData[sample]));
+            (void)delayLine.popSample(channel);
+            threshold = thresholdSmoothed[channel].getNextValue();
+            gain = gainSmoothed[channel].getNextValue();
+            drive = driveSmoothed[channel].getNextValue();
+            hold = holdSmoothed[channel].getNextValue();
+            release = releaseSmoothed[channel].getNextValue();
+
+            limiters[channel].setThreshold(threshold);
+            limiters[channel].setHoldMs(hold);
+            limiters[channel].setReleaseMs(release);
+
+            auto newSample = channelData[sample];
+
+            auto val = static_cast<T>(limiters[channel].sample(newSample));
+            if (!isLinked)
+            {
+                val *= 1 / threshold;
+                val *= gain;
+            }
+
+            channelData[sample] = val;
+
+            auto gr = limiters[channel].gainReduction;
+            gainReduction.store((float)gr, std::memory_order_release);
+        }
+    }
+}
+
+template <typename T>
+void RipuLimiterAudioProcessor::processBlockBypassedInternal(
+    juce::AudioBuffer<T>& buffer, [[maybe_unused]] juce::MidiBuffer& midiMessages
+)
+{
+    for (int channel = 0; channel < getTotalNumInputChannels(); ++channel)
+    {
+        auto* channelData = buffer.getWritePointer(channel);
+        for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
+        {
+            delayLine.pushSample(channel, static_cast<double>(channelData[sample]));
+            channelData[sample] = static_cast<T>(delayLine.popSample(channel));
+        }
     }
 }
 
 //==============================================================================
-bool AudioPluginAudioProcessor::hasEditor() const
+bool RipuLimiterAudioProcessor::hasEditor() const
 {
     return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
+juce::AudioProcessorEditor* RipuLimiterAudioProcessor::createEditor()
 {
-    return new AudioPluginAudioProcessorEditor (*this);
+    return new RipuLimiterAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void AudioPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void RipuLimiterAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-    juce::ignoreUnused (destData);
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml(state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
-void AudioPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void RipuLimiterAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-    juce::ignoreUnused (data, sizeInBytes);
+    std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+
+    if (xmlState != nullptr)
+        if (xmlState->hasTagName(apvts.state.getType()))
+            apvts.replaceState(juce::ValueTree::fromXml(*xmlState));
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new AudioPluginAudioProcessor();
+    return new RipuLimiterAudioProcessor();
 }
